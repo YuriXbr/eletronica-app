94f5667a733c28b22945422af0a4a9d1a5282b49
Adriel-Silva <adriel.a0608@gmail.com>
2025-06-16 23:26:24 -0300

    Página de resistores criada

---
+++ app/(tabs)/resistor.tsx
@@ -0,0 +1,16 @@
+import 'katex/dist/katex.min.css';
+import Latex from 'react-latex-next';
+
+
+export default function Resistor() {
+
+    return (
+        <>
+            <div className='m-3 p-3 bg-zinc-400 w-auto h-auto flex flex-row space-x-5'>
+                <h1 className=''>TEST PAGE</h1>
+                <Latex>{`$1,6\\times 10^{-19} C$`}</Latex>
+
+            </div>
+        </>
+    )
+}
